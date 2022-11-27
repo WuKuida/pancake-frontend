3f36c032edfd404b9ebbb5754aa03c57222ed62c
a <a>
2022-11-27 22:10:39 +0800

    udpate

--- src/config/constants/exchange.ts
+++ src/config/constants/exchange.ts
@@ -5,7 +5,7 @@ import { ChainTokenList } from './types'
 
 export const ROUTER_ADDRESS = {
   [ChainId.BSC]: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
-  [ChainId.BSC_TESTNET]: '0xFD70905447Eba00f785E63a41580C3C17bF69F4B',
+  [ChainId.BSC_TESTNET]: '0x1f55053b314EF7B0bACBC53E0927ecC8782a0456',
 }
 
 // used to construct intermediary pairs for trading
